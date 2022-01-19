1f6ea791dc6c15b690c5ec95f415d9af24080175
debs <dfd2@cin.ufpe.br>
2022-01-19 20:11:25 -0300

    Authenticate pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { CreateRoom } from "./pages/CreateRoom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/create" element={<CreateRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
